7fb81f5dca3cff7033972847cb67455d6519fcad
Gaia Di Lorenzo <gdilorenzo@ethz.ch>
2025-04-29 12:36:02 +0200

    feat: training scripts

---
+++ scripts/train_val/train_scene_graph_loc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+args=("$@")
+
+# Set environment variables
+export VLSG_SPACE="$(pwd)"
+export RESUME_DIR="$VLSG_TRAINING_OUT_DIR"
+
+# get output directory argument if it exists
+for i in "$@"
+do
+case $i in
+    -o=*|--output_dir=*)
+    export VLSG_TRAINING_OUT_DIR="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+# Set output directory
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+export VLSG_TRAINING_OUT_DIR="$SCRATCH/training_scene_graph_loc/$timestamp"
+
+# Initialize conda and activate the environment
+source .venv/bin/activate
+
+# Navigate to VLSG space
+cd "$VLSG_SPACE" || { echo "Failed to change directory to $VLSG_SPACE"; exit 1; }
+export PYTHONPATH="$VLSG_SPACE:$PYTHONPATH:$VLSG_SPACE/dependencies/gaussian-splatting"
+
+# Run training script
+python src/trainval/train_scene_graph_loc.py --config scripts/train_val/train_scene_graph_loc.yaml --log_steps 10 output_dir=\"$VLSG_TRAINING_OUT_DIR\" ${args[@]}
